2d36720cfa1e033257fedbb2e3d00b9aea91c68b
Alex <AlexZhu2001@stu.xupt.edu.cn>
2023-10-09 19:08:08 +0800

    Add enc_wbi and get_mixin_key functions Add tests for enc_wbi and get mixin_key

--- bilibili_api/wbi.py
+++ bilibili_api/wbi.py
@@ -1,7 +1,11 @@
 import json
+import time
+import urllib
 from datetime import datetime
+from functools import reduce
 from typing import Optional
 
+from Crypto.Hash import MD5
 from loguru import logger
 
 from local_storage import LocalStorage
@@ -12,6 +16,12 @@ WBI_CACHE_FILE = r'.wbi_cache'
 WBI_IMG_KEY = 'wbi_img'
 WBI_SUB_KEY = 'wbi_sub'
 WBI_TS_KEY = 'wbi_ts'
+MIXIN_ENC_TABLE = [
+    46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
+    33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
+    61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
+    36, 20, 34, 44, 52
+]
 
 
 def _datetiem_to_str(dt: datetime) -> str:
@@ -32,6 +42,15 @@ def _time_check(dt: str) -> bool:
     return y == ty and m == tm and d == td
 
 
+def process_key(img: str, sub: str) -> tuple[str, str]:
+    def proc_fun(a: str) -> str:
+        a = a.rsplit('/', 1)[1]
+        a = a.split('.', 1)[0]
+        return a
+
+    return proc_fun(img), proc_fun(sub)
+
+
 def get_wbi() -> Optional[tuple[str, str]]:
     wbi_cache = LocalStorage(WBI_CACHE_FILE)
     ts = wbi_cache.get(WBI_TS_KEY, None)
@@ -44,8 +63,8 @@ def get_wbi() -> Optional[tuple[str, str]]:
         reply = session.get(UrlData.URL_NAV)
         obj = json.loads(reply.text)
         wbi = obj['data']['wbi_img']
-        img = wbi['img_url']
-        sub = wbi['sub_url']
+        img, sub = wbi['img_url'], wbi['sub_url']
+        img, sub = process_key(img, sub)
         wbi_cache.put(WBI_IMG_KEY, img)
         wbi_cache.put(WBI_SUB_KEY, sub)
         wbi_cache.put(WBI_TS_KEY, _datetiem_to_str(datetime.now()))
@@ -53,3 +72,24 @@ def get_wbi() -> Optional[tuple[str, str]]:
         return img, sub
     except (IOError, KeyError, ValueError):
         return None
+
+
+def get_mixin_key(img_key: str, sub_key: str) -> str:
+    cat = img_key + sub_key
+    return reduce(lambda s, i: s + cat[i], MIXIN_ENC_TABLE, '')[:32]
+
+
+def enc_wbi(params: dict, img_key: str, sub_key: str) -> dict:
+    mixin_key = get_mixin_key(img_key, sub_key)
+    cur_time = round(time.time())
+    params['wts'] = cur_time
+    params = dict(sorted(params.items()))
+    params = {
+        k: ''.join(filter(lambda chr: chr not in "!'()*", str(v)))
+        for k, v
+        in params.items()
+    }
+    query = urllib.parse.urlencode(params)  # 序列化参数
+    wbi_sign = MD5.new((query + mixin_key).encode('utf8')).hexdigest().lower()
+    params['w_rid'] = wbi_sign
+    return params
--- tests/test_wbi.py
+++ tests/test_wbi.py
@@ -1,10 +1,11 @@
-import copy
+import inspect
 import os.path
+import time
 import unittest
 
 from loguru import logger
 
-from bilibili_api.wbi import get_wbi, WBI_CACHE_FILE, WBI_TS_KEY
+from bilibili_api.wbi import get_wbi, WBI_CACHE_FILE, WBI_TS_KEY, process_key, get_mixin_key, enc_wbi
 from local_storage import LocalStorage
 
 
@@ -14,6 +15,26 @@ def clean():
 
 
 class TestWbi(unittest.TestCase):
+    pk_original = ("https://i0.hdslb.com/bfs/wbi/653657f524a547ac981ded72ea172057.png",
+                   "https://i0.hdslb.com/bfs/wbi/6e4909c702f846728e64f6007736a338.png")
+    pk_corr = ("653657f524a547ac981ded72ea172057", "6e4909c702f846728e64f6007736a338")
+    pk_mixin = "72136226c6a73669787ee4fd02a74c27"
+    params = {
+        'bar': '514',
+        'foo': '114',
+        'zab': '1919810'
+    }
+    after_wbi = {
+        'bar': '514',
+        'foo': '114',
+        'wts': '1684746387',
+        'zab': '1919810',
+        'w_rid': '90efcab09403023875b8516f07e9f9de'
+    }
+    ts_less = '1#2'
+    ts_more = '1#2#3#4#5'
+    NOT_ALLOWED_URL = 'https://example.com/404.html'
+
     def test_get_wbi(self):
         val = get_wbi()
         self.assertTrue(val is not None)
@@ -36,15 +57,31 @@ class TestWbi(unittest.TestCase):
         UrlData.URL_NAV = tmp
 
     def test_ts_invalid(self):
+        _ = get_wbi()
         wbi_cache = LocalStorage(WBI_CACHE_FILE)
-        tmp = copy.copy(wbi_cache)
-        wbi_cache.put(WBI_TS_KEY, '1#2')
+        wbi_cache.put(WBI_TS_KEY, self.ts_less)
         wbi_cache.save()
         _ = get_wbi()
         self.assertLogs(logger, "WARNING")
-        wbi_cache.put(WBI_TS_KEY, '1#2#3#4#5')
+        wbi_cache.put(WBI_TS_KEY, self.ts_more)
         wbi_cache.save()
         _ = get_wbi()
         self.assertLogs(logger, "WARNING")
-        tmp.save()
+        clean()
 
+    def test_process_key(self):
+        ans = process_key(*self.pk_original)
+        self.assertEqual(self.pk_corr, ans)
+
+    def test_get_mixin_key(self):
+        ans = process_key(*self.pk_original)
+        mixin = get_mixin_key(*ans)
+        self.assertEqual(mixin, self.pk_mixin)
+
+    def test_enc_wbi(self):
+        bak = time.time
+        inspect.currentframe()
+        time.time = lambda: 1684746387
+        ans = enc_wbi(self.params, *self.pk_corr)
+        self.assertDictEqual(ans, self.after_wbi)
+        time.time = bak
